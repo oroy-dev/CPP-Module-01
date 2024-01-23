7d84abda341445ea32abf92ec76800ce1e52f98d
Olivier Roy <olivierroy123@gmail.com>
2024-01-23 13:13:59 -0500

    ex04 try

---
+++ ex04/replace.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 12:28:35 by oroy              #+#    #+#             */
+/*   Updated: 2024/01/23 13:05:48 by oroy             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_H
+# define REPLACE_H
+
+# include <iostream>
+# include <fstream>
+
+class replace
+{
+private:
+	/* data */
+public:
+	replace(/* args */);
+	~replace();
+};
+
+#endif
