b0372001f68a3c6aa9f54bdae2e06a19218bcddf
Olivier Roy <olivierroy123@gmail.com>
2024-01-23 20:19:05 -0500

    Pretty much done with ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,23 +6,55 @@
 /*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 12:28:08 by oroy              #+#    #+#             */
-/*   Updated: 2024/01/23 13:13:48 by oroy             ###   ########.fr       */
+/*   Updated: 2024/01/23 20:18:43 by oroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "replace.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void	create_output(std::string &s1, std::string &s2, std::ifstream &ifs, std::ofstream &ofs)
+{
+	std::string::size_type	pos;
+	std::string				line;
+
+	while (ifs.good())
+	{
+		std::getline(ifs, line);
+		while (!s2.empty() && (pos = line.find(s1)) != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+		}
+		if (ifs.eof())
+			ofs << line;
+		else
+			ofs << line << std::endl;
+	}
+}
 
 int	main(int argc, char **argv)
 {
+	std::string		s1, s2;
+	std::ifstream	ifs;
+	std::ofstream	ofs;
+
 	if (argc == 1)
 	{
-		return (0);
+		std::cout << "[ERROR]	Please provide at least an input file:" << std::endl;
+		std::cout << "	<filename>, <string to find>, <string used as replacement>" << std::endl;
+		return (1);
 	}
-	std::ifstream	ifs("");
-	std::ofstream	ofs;
-	if (argc == 4)
+	else if (argc >= 4)
 	{
-		
+		s1 = argv[2];
+		s2 = argv[3];
 	}
+	ifs.open(argv[1]);
+	ofs.open("out");
+	create_output(s1, s2, ifs, ofs);
+	ifs.close();
+	ofs.close();
 	return (0);
 }
